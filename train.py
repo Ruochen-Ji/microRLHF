a8efa0f490f3e66391b1ede2b08067cdd728f3b8
Ruochen <jrc7@bu.edu>
2026-01-03 13:08:59 -0800

    Add comments to training code

--- train.py
+++ train.py
@@ -126,6 +126,8 @@ def get_batch(split):
         data = np.memmap(os.path.join(data_dir, 'train.bin'), dtype=np.uint16, mode='r')
     else:
         data = np.memmap(os.path.join(data_dir, 'val.bin'), dtype=np.uint16, mode='r')
+    # Initializing an array with length of batch_size and random start index.
+    # We use this array to randonly sample a batch of data. 
     ix = torch.randint(len(data) - block_size, (batch_size,))
     x = torch.stack([torch.from_numpy((data[i:i+block_size]).astype(np.int64)) for i in ix])
     y = torch.stack([torch.from_numpy((data[i+1:i+1+block_size]).astype(np.int64)) for i in ix])
@@ -242,6 +244,7 @@ def estimate_loss():
             X, Y = get_batch(split)
             with ctx:
                 logits, loss = model(X, Y)
+            # Convert the loss from a tensor to a python number.
             losses[k] = loss.item()
         out[split] = losses.mean()
     model.train()
